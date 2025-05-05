a2290b5746edb276e8e89aa59703558ccb1f7575
Robert <robert.szalapak@gmail.com>
2025-05-05 15:16:42 +0200

    rewriting project on vue - auth form

---
+++ src/components/Loader/Loader.vue
@@ -0,0 +1,12 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script lang="ts">
+export default {
+  
+}
+</script>
+<template>
+  <div className="Loader" data-cy="Loader">
+    <div className="Loader__content" />
+  </div>
+</template>
+
---
+++ src/components/PostsList.vue
@@ -0,0 +1,54 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      posts: [],
+    }
+  },
+  mounted() {
+    
+  }
+}
+
+
+</script>
+
+
+
+<template>
+
+
+    <div data-cy="PostsList">
+      <p className="title">Posts:</p>
+      <table className="table is-fullwidth is-striped is-hoverable is-narrow">
+        <thead>
+          <tr className="has-background-link-light">
+            <th>#</th>
+            <th>Title</th>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <th> </th>
+          </tr>
+        </thead>
+        <tbody>
+          {{posts}}
+            {/* return (
+              <tr data-cy="Post" key={post.id}>
+                <td data-cy="PostId">{post.id}</td>
+                <td data-cy="PostTitle">{post.body}</td>
+                <td className="has-text-right is-vcentered">
+                  <button
+                    type="button"
+                    data-cy="PostButton"
+                    className="button is-link is-light"
+                  >
+                    {selectedPost?.id === post.id ? 'Close' : 'Open'}
+                  </button>
+                </td>
+              </tr>
+            ); */}
+
+        </tbody>
+      </table>
+    </div>
+</template>
+
